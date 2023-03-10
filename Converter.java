c21d6d2ff31a7da7babdb3ba996904b97aabd9e1
unan-bol <127199237+unan-bol@users.noreply.github.com>
2023-03-10 19:07:29 +0300

    Add files via upload

---
+++ Converter.java
@@ -0,0 +1,9 @@
+class Converter{
+    int convertToKm(int steps){
+        return (steps * 75)/100000;
+    }
+
+    int convertStepsToKilocalories(int steps){
+        return (steps * 50)/100000;
+    }
+}
