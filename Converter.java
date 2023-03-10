d842bed21586b9ad8ad42c639fec61a3f4898f67
unan-bol <127199237+unan-bol@users.noreply.github.com>
2023-03-10 19:40:44 +0300

    Delete Converter.java

--- Converter.java
+++
@@ -1,9 +0,0 @@
-class Converter{
-    int convertToKm(int steps){
-        return (steps * 75)/100000;
-    }
-
-    int convertStepsToKilocalories(int steps){
-        return (steps * 50)/100000;
-    }
-}
